1a5d3e2d37735364499be3e5fdd7f2d383bec51b
fernandomdureira <132229089+fernandomdureira@users.noreply.github.com>
2023-04-30 21:38:47 +0100

    atualizei hoje os arquivos

---
+++ main.js
@@ -0,0 +1,37 @@
+const menuicon = document.getElementById("menu-icon");
+const slideoutMenu = document.getElementById("slideout-menu");
+const searchIcon = document.getElementById("search-icon");
+const searchBox = document.getElementById("searchbox");
+
+
+searchIcon.addEventListener('click', function() {
+
+	if (searchBox.style.top == '72px') {
+
+		searchBox.style.top == '24px';
+
+		searchBox.style.pointerEvents = 'none';
+
+	} else{
+		searchBox.style.top = '72px';
+		searchBox.style.top.pointerEvents = 'auto';
+	}
+});
+
+	// body...
+
+
+menuicon.addEventListener('click' , function(){
+	if (slideoutMenu.style.opacity == "1") {
+
+		slideoutMenu.style.opacity ='0';
+		slideoutMenu.style.pointerEvents ='none';
+	}else{
+
+		slideoutMenu.style.opacity ='1';
+		slideoutMenu.style.pointerEvents ='auto';
+	}
+})
+
+
+
